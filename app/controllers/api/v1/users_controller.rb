5b55d6ec89442922f8d8f29a60a1472726ac140e
Andrey Sechin <sechinandreyv@gmail.com>
2022-12-07 21:12:14 +0200

    init

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::UsersController < Api::V1::BaseController
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+end
